cb29e5497129a8a8d9edd3cbdc381e9fb9d360b4
Ambika <ambika3223@gmail.com>
2021-09-24 21:17:58 +0200

    add dashboard

---
+++ src/dashboard/Dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+class Dashboard extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Welcome to dashboard</h1>
+            </div>
+        )
+    }
+
+
+}
+
+export default Dashboard;
