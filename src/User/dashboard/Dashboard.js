ec36564d6fb9f8fc01fd99cba841544b1e480f13
Vishal Kalola <vishalkalola196@gmail.com>
2021-10-02 23:41:37 +0200

    resolve bugs

--- src/User/dashboard/Dashboard.js
+++ src/User/dashboard/Dashboard.js
@@ -7,15 +7,15 @@ import Gallary from '../gallaryView/Gallary';
 
 class Dashboard extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             details: null
         }
-        this.gridSelect= this.gridSelect.bind(this);
+        this.gridSelect = this.gridSelect.bind(this);
     }
-   
-    gridSelect(resturant){
+
+    gridSelect(resturant) {
         this.setState({
             details: resturant
         })
@@ -23,20 +23,22 @@ class Dashboard extends React.Component {
 
     render() {
         return (
-            <div className={`vh-100 h-100`} style={{backgroundColor:'rgba(247,247,247,1)'}}>
-                <div style={{height:'50px',backgroundColor:'white',marginBottom:'5px'}}>
-                    <h4 style={{textAlign:'center', color:'orange', paddingTop:'10px'}}>Resturant Review</h4>
+            <div className={`vh-100 h-100`} style={{ backgroundColor: 'rgba(247,247,247,1)' }}>
+                <div style={{ height: '50px', backgroundColor: 'white', marginBottom: '5px' }}>
+                    <h4 style={{ textAlign: 'center', color: 'orange', paddingTop: '10px' }}>Resturant Review</h4>
                 </div>
-            <Row>
-                <Col md={8} style={{backgroundColor:'rgba(247,247,247,1)', height:'100vh', overflow:'auto'}}>
-                    <Gallary select={this.gridSelect}/>
-                </Col>
-                {this.state.details !== null  && <Col md={4} style={{backgroundColor:'white'}}>
-                    <Details resturant={this.state.details}/>
-                </Col>}
-                
-            </Row>
-        </div>
+                <Row>
+                    <Col md={8} style={{ backgroundColor: 'rgba(247,247,247,1)', height: '100vh', overflow: 'auto' }}>
+                        <Gallary select={this.gridSelect} />
+                    </Col>
+                    {
+                        this.state.details !== null && 
+                        (<Col md={4} style={{ backgroundColor: 'white' }}>
+                            <Details resturant={this.state.details} />
+                        </Col>)
+                    }
+                </Row>
+            </div>
         )
     }
 }
--- src/User/detailView/Details.js
+++ src/User/detailView/Details.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 import images from '../../img/images.jpg';
 import './detail.css';
 import StarRatings from 'react-star-ratings';
-import { FaLocationArrow, FaPhoneAlt, FaStar} from 'react-icons/fa';
+import { FaLocationArrow, FaPhoneAlt, FaStar } from 'react-icons/fa';
 import { Col, Row } from 'react-bootstrap';
 
 
@@ -12,37 +12,41 @@ class Details extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            resturant:this.props.resturant,
-            reviews:[],
+            resturant: this.props.resturant,
+            reviews: [],
             disabled: true
-        }  
+        }
         this.dateConversion = this.dateConversion.bind(this)
     }
 
+    componentDidMount() {
+        this.reviewsApi();
+    }
+
     componentDidUpdate(prevProps, prevState) {
-       
+
         if (prevProps.resturant !== this.props.resturant) {
             this.setState({
-                resturant:this.props.resturant
+                resturant: this.props.resturant
             })
-          this.reviewsApi();
+            this.reviewsApi();
         }
     }
 
     changeRating = (event) => {
         alert('hello')
     }
-     
-    dateConversion = (dateUnix) =>{
+
+    dateConversion = (dateUnix) => {
         const date = new Date(dateUnix * 1000);
         return date.toLocaleString()
     }
-    reviewsApi =() => {
+    reviewsApi = () => {
         const requestOptions = {
             method: 'GET',
             headers: { 'Content-Type': 'application/json', 'Authorization': 'token ' + localStorage.getItem('token'), }
         };
-        fetch('https://andesrestaurant.herokuapp.com/api/restaurants/'+this.props.resturant.id+'/reviews', requestOptions)
+        fetch('https://andesrestaurant.herokuapp.com/api/restaurants/' + this.props.resturant.id + '/reviews', requestOptions)
             .then(response => {
                 if (!response.ok) {
                     throw new Error(response.status);
@@ -52,9 +56,9 @@ class Details extends React.Component {
                 }
             })
             .then(data => {
-                console.log('data',data)
+                console.log('data', data)
                 this.setState({
-                    reviews:data
+                    reviews: data
                 })
             })
             .catch(error => {
@@ -66,40 +70,39 @@ class Details extends React.Component {
 
     render() {
         return (
-            <div> 
-               <div>
-                    <img style={{ width: '100%', height: '200px', paddingTop:'15px'}} src={images} alt='pic' />
-                    <li style={{  listStyleType: 'none', color: 'orange',fontSize:'1.5rem' }}><b>{this.state.resturant.name}</b></li>
-                    <li style={{  listStyleType: 'none', color: 'gray' }}><FaLocationArrow color='blue' className="mr-2"/>{this.state.resturant.address}</li>
-                    <li style={{  listStyleType: 'none', color: 'gray' }}><FaPhoneAlt color='red' className="mr-2"/>{this.state.resturant.contact}</li>
-                    <li style={{ listStyleType: 'none', color: 'gray' }}><FaStar color='orange' className="mr-2"/>{this.state.resturant.avg.toFixed(1)}</li> 
+            <div>
+                <div>
+                    <img style={{ width: '100%', height: '200px', paddingTop: '15px' }} src={images} alt='pic' />
+                    <li style={{ listStyleType: 'none', color: 'orange', fontSize: '1.5rem' }}><b>{this.state.resturant.name}</b></li>
+                    <li style={{ listStyleType: 'none', color: 'gray' }}><FaLocationArrow color='blue' className="mr-2" />{this.state.resturant.address}</li>
+                    <li style={{ listStyleType: 'none', color: 'gray' }}><FaPhoneAlt color='red' className="mr-2" />{this.state.resturant.contact}</li>
+                    <li style={{ listStyleType: 'none', color: 'gray' }}><FaStar color='orange' className="mr-2" />{this.state.resturant.avg.toFixed(1)}</li>
                     <h4 className='pt-2'>Reviews</h4>
-                    <div className="overflow-auto" style={{height:'285px'}} > 
-                       {this.state.reviews.map((review, index) =>
-                        {
-                            return (<div className='border-bottom' key={'review'+ index}>
-                                <li style={{ listStyleType: 'none'}}><b>{review.user.first_name + ' '+ review.user.last_name}</b></li>
-                                <li style={{ listStyleType: 'none'}}>
-                                <Row style={{margin:'0px'}}>
-                                    <Col md={3.5} >
-                                    <StarRatings
-                                        rating={review.rating}
-                                        starRatedColor="orange"
-                                        numberOfStars={5}
-                                        name='rating'
-                                        starDimension='20px'
-                                        starSpacing="2px"
-                                    />
-                                    </Col>
-                                    <Col md={8} > <h6 style={{marginTop:'7px'}}> {this.dateConversion(review.date)} </h6></Col>
-                                </Row></li>
-                                <li style={{listStyleType: 'none'}}><label>{review.comment}</label></li>  
-                           </div>)
+                    <div className="overflow-auto" style={{ height: '285px' }} >
+                        {this.state.reviews.map((review, index) => {
+                            return (<div className='border-bottom' key={'review' + index}>
+                                <li style={{ listStyleType: 'none' }}><b>{review.user.first_name + ' ' + review.user.last_name}</b></li>
+                                <li style={{ listStyleType: 'none' }}>
+                                    <Row style={{ margin: '0px' }}>
+                                        <Col md={3.5} >
+                                            <StarRatings
+                                                rating={review.rating}
+                                                starRatedColor="orange"
+                                                numberOfStars={5}
+                                                name='rating'
+                                                starDimension='20px'
+                                                starSpacing="2px"
+                                            />
+                                        </Col>
+                                        <Col md={8} > <h6 style={{ marginTop: '7px' }}> {this.dateConversion(review.date)} </h6></Col>
+                                    </Row></li>
+                                <li style={{ listStyleType: 'none' }}><label>{review.comment}</label></li>
+                            </div>)
                         }
-                       )} 
- 
-                   </div>
-                </div>      
+                        )}
+
+                    </div>
+                </div>
             </div>
         )
     }
