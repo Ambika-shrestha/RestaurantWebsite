4d552c8189dd341f5620e55735270cbd82eb5d29
Ambika <ambika3223@gmail.com>
2021-10-12 19:11:01 +0200

    Index added into table

--- src/Admin/UserList/UserList.js
+++ src/Admin/UserList/UserList.js
@@ -73,7 +73,7 @@ class UserList extends React.Component {
                     {this.state.userList.map((user, index) => {
                         return (
                             <tr>
-                                <td>{user.id}</td>
+                                <td>{index+1}</td>
                                 <td>{user.first_name} {user.last_name}</td>
                                 <td>{user.email}</td>
                                 <td>{user.username}</td>
