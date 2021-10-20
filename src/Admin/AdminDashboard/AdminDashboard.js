ba37853b80f7a9266474cf68e05ef8d03cdbb59a
Ambika <ambika3223@gmail.com>
2021-10-20 19:23:09 +0200

    new folders and review folder added

--- src/Admin/AdminDashboard/AdminDashboard.js
+++ src/Admin/AdminDashboard/AdminDashboard.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import { FaPlusCircle } from 'react-icons/fa';
-import UserList from '../UserList/UserList'
-import AddUser from '../AddUser/AddUser';
-import UserEdit from '../UserEdit/UserEdit';
+import UserList from '../User/UserList/UserList'
+import AddUser from '../User/AddUser/AddUser';
+import UserEdit from '../User/UserEdit/UserEdit';
+import ResturantList from '../Resturant/ResturantList/ResturantList';
+import ReviewsList from '../Reviews/ReviewList/ReviewsList';
 
 
 class AdminDashboard extends React.Component {
@@ -13,9 +15,13 @@ class AdminDashboard extends React.Component {
         this.state = {
             modalOpened: false,
             modalEditOpened: false,
+            selectedIndex: 1,
             user: null
         }
         this.addUserBtn = this.addUserBtn.bind(this)
+        this.selectedTab = this.selectedTab.bind(this)
+        this.addResturantBtn = this.addResturantBtn.bind(this)
+        this.addReviewsBtn = this.addReviewsBtn.bind(this)
     }
 
     addUserBtn = () => {
@@ -24,6 +30,19 @@ class AdminDashboard extends React.Component {
         });
     }
 
+    addResturantBtn = () =>{
+
+    }
+    
+    addReviewsBtn = () =>{
+        
+    }
+
+    selectedTab = (id) => {
+        this.setState({
+            selectedIndex: id,
+        })
+    }
     userEditPopUpToggle = (editPopuser) => {
         this.setState({
             modalEditOpened: !this.state.modalEditOpened,
@@ -36,28 +55,52 @@ class AdminDashboard extends React.Component {
                 <div className='m-5 position-absolute start-0 top-0 end-0 bottom-0'>
                     <ul className="nav nav-tabs border-0 ">
                         <div className="nav-item me-2">
-                            <a className="nav-link text-white d-flex align-items-center "
-                                style={{ borderRadius: '15px 15px 0px 0px', height: '60px', width: '130px', backgroundColor: 'rgba(57, 109, 229, 1)' }} aria-current="page" href="#">User
-                                <Button className='ms-5 border-0 d-flex justify-content-center align-items-center p-0'
+                            <div className='nav-link d-flex justify-content-center align-items-center'
+                                style={{ borderRadius: '15px 15px 0px 0px', height: '60px', backgroundColor: 'rgba(57, 109, 229, 1)' }}>
+                                <Button onClick={e => this.selectedTab(1)} className=" border-0  me-2 text-white d-flex align-items-center"
+                                    style={{ height: '60px', backgroundColor: 'rgba(57, 109, 229, 1)' }}>
+                                    User
+                                </Button>
+                                <Button className='me-2 border-0 d-flex justify-content-center align-items-center p-0'
                                     style={{ backgroundColor: 'rgba(57, 109, 229, 1)', height: '20px', width: '20px' }}
                                     onClick={this.addUserBtn}>
-                                    <FaPlusCircle /> </Button></a>
+                                    <FaPlusCircle />
+                                </Button>
+                            </div>
                         </div>
                         <li className="nav-item me-2">
-                            <a className="nav-link text-white d-flex align-items-center "
-                                style={{ borderRadius: '15px 15px 0px 0px', height: '60px', width: '130px', backgroundColor: 'rgba(57, 109, 229, 1)' }} href="#">Resturant
-                                <Button className='ms-3 border-0 d-flex justify-content-center align-items-center p-0' style={{ backgroundColor: 'rgba(57, 109, 229, 1)', height: '20px', width: '20px' }}>
-                                    <FaPlusCircle /> </Button></a>
+                            <div className='nav-link d-flex justify-content-center align-items-center'
+                                style={{ borderRadius: '15px 15px 0px 0px', height: '60px', backgroundColor: 'rgba(57, 109, 229, 1)' }}>
+                                <Button onClick={e => this.selectedTab(2)} className=" border-0 me-2 text-white d-flex align-items-center"
+                                    style={{ height: '60px', backgroundColor: 'rgba(57, 109, 229, 1)' }}>
+                                    Resturants
+                                </Button>
+                                <Button className='me-2 border-0 d-flex justify-content-center align-items-center p-0'
+                                    style={{ backgroundColor: 'rgba(57, 109, 229, 1)', height: '20px', width: '20px' }}
+                                    onClick={this.addResturantBtn}>
+                                    <FaPlusCircle />
+                                </Button>
+                            </div>
                         </li>
                         <li className="nav-item me-2">
-                            <a className="nav-link text-white d-flex align-items-center "
-                                style={{ borderRadius: '15px 15px 0px 0px', height: '60px', width: '130px', backgroundColor: 'rgba(57, 109, 229, 1)' }} href="#">Admin
-                                <Button className='ms-4 border-0 d-flex justify-content-center align-items-center p-0' style={{ backgroundColor: 'rgba(57, 109, 229, 1)', height: '20px', width: '20px' }}>
-                                    <FaPlusCircle /> </Button></a>
+                            <div className='nav-link d-flex justify-content-center align-items-center'
+                                style={{ borderRadius: '15px 15px 0px 0px', height: '60px', backgroundColor: 'rgba(57, 109, 229, 1)' }}>
+                                <Button onClick={e => this.selectedTab(3)} className="border-0  me-2 text-white d-flex align-items-center"
+                                    style={{ height: '60px', backgroundColor: 'rgba(57, 109, 229, 1)' }}>
+                                    Reviews
+                                </Button>
+                                <Button className='me-2 border-0 d-flex justify-content-center align-items-center p-0'
+                                    style={{ backgroundColor: 'rgba(57, 109, 229, 1)', height: '20px', width: '20px' }}
+                                    onClick={this.addReviewsBtn}>
+                                    <FaPlusCircle />
+                                </Button>
+                            </div>
                         </li>
                     </ul>
                     <div className='bg-white shadow-sm' style={{ height: '95%' }}>
-                        <UserList userEditPopUpToggle={this.userEditPopUpToggle} />
+                        {this.state.selectedIndex === 1 && <UserList userEditPopUpToggle={this.userEditPopUpToggle} />}
+                        {this.state.selectedIndex === 2 && <ResturantList />}
+                        {this.state.selectedIndex === 3 && <ReviewsList />}
                     </div>
                 </div>
                 {this.state.modalOpened && <AddUser toggle={this.addUserBtn} />}
---
+++ src/Admin/Reviews/ReviewList/ReviewsList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+class ReviewsList extends React.Component{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+           <div>
+               <p>Reviews List</p>
+           </div>
+        )
+    }
+}
+
+export default ReviewsList;
